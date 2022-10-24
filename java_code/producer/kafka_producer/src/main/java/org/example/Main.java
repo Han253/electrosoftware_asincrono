81990771a0290ab7a591544763916b3a5ca0ec8f
han253 <han315730@gmail.com>
2022-10-24 02:03:29 -0500

    add consumer

--- java_code/producer/kafka_producer/src/main/java/org/example/Main.java
+++ java_code/producer/kafka_producer/src/main/java/org/example/Main.java
@@ -2,7 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         ProducerDemo producer = new ProducerDemo();
         producer.SendData("Hola mundo desde Java");
     }
