a176cc0c388068e317ab604b494e2646e75b99e1
han253 <han315730@gmail.com>
2023-04-28 13:16:04 -0500

    add amq example

---
+++ amqp/ejemplo_1_spring/producer/src/main/java/com/example/producer/config/RabbitConfiguration.java
@@ -0,0 +1,47 @@
+package com.example.producer.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/*Ejemplo de configuración para Spring Boog con Rabbit MQ
+ * Código basado en el ejemplo presentado en: 
+ * https://www.javaguides.net/2022/07/spring-boot-rabbitmq-producer-and-consumer-example.html 
+ */
+
+
+@Configuration
+public class RabbitConfiguration {
+
+    @Value("${rabbitmq.queue.name}")
+    private String queue;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    // spring bean for rabbitmq queue
+    @Bean
+    public Queue queue(){
+        return new Queue(queue);
+    }
+
+    // spring bean for rabbitmq exchange
+    @Bean
+    public TopicExchange exchange(){
+        return new TopicExchange(exchange);
+    }
+
+    // binding between queue and exchange using routing key
+    @Bean
+    public Binding binding(){
+        return BindingBuilder
+                .bind(queue())
+                .to(exchange())
+                .with(routingKey);
+    }  
+	
+}
